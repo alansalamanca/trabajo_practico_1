4816ad43a0789edde80c9412c53ec92e527da2fc
alan <alan@DESKTOP-00CC07C>
2024-04-04 15:19:10 -0300

    ejercicio1 tp1 completo

--- tp1_SalamancaAlan/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_SalamancaAlan/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,8 +3,15 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenido a Programacion Visual");
-
+		int numero1 = 8;
+		int numero2 = 5;
+		int numero3 = 1;
+		int numero4 = 9;
+		int numero5 = 20;
+		double promedio = (double) (numero1 + numero2 + numero3 
+				                   + numero4 + numero5)/5;
+		
+		System.out.println(promedio);
 	}
 
 }
