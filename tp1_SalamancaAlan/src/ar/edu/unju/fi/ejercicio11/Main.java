416153c44527d0cbbfa9855ed73a02d8c9457fe4
alan <alan@DESKTOP-00CC07C>
2024-04-18 03:30:51 -0300

    Resolucion ejercicio 11

---
+++ tp1_SalamancaAlan/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j, i = 2, calculo = 0 ;
+		
+		for (j = 40; j > 0; j--) {
+			calculo = j * i;
+			System.out.print(calculo + " ");
+			i++;
+		}
+	}
+
+}
